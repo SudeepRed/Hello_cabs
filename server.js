36703d7a567115280f98eb66bbc452c64c5381f3
expnoob <sudeep19313@iiitd.ac.in>
2022-01-09 04:10:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import driver_route from "./src/api/v1/routes/driver.js";
 import ride_route from "./src/api/v1/routes/ride.js";
 import rider_route from "./src/api/v1/routes/rider.js";
-import r from "./src/api/v1/models/ride.js"
+
 const app = express();
 app.use(express.json());
 app.listen("3001", () => {
